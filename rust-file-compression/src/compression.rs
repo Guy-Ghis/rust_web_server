0827a50b722ec6434a967ce5cbb650991e104bea
Guy-Ghis <tissongguyghislain001@gmail.com>
2025-03-18 16:06:00 +0100

    update: ingnore unused variables

--- rust-file-compression/src/compression.rs
+++ rust-file-compression/src/compression.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 
 pub fn compress() {
     let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-    e.write_all(b"foo");
-    e.write_all(b"bar");
-    let compressed_bytes = e.finish();
+    let _ = e.write_all(b"foo");
+    let _ = e.write_all(b"bar");
+    let _compressed_bytes = e.finish();
 }
